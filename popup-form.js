4000a0dc296a927c07f4526feace0a069a4e7863
Vaibhavi_g <34022504+vaibhavig12@users.noreply.github.com>
2024-05-21 13:03:51 +0530

    Add files via upload
    Added submitForm function to handle form submission
    
    In this commit, I added a new JavaScript function named submitForm to handle the submission of form data to a Google Apps Script web app. This function retrieves form data using FormData, sends it asynchronously to the specified URL using XMLHttpRequest, and displays an alert to the user upon successful submission.

---
+++ popup-form.js
@@ -0,0 +1,37 @@
+// popup form to appear
+// Get the form and the close button
+const formPopup = document.getElementById('enquiryFormPopup');
+const closeButton = document.getElementById('closeBtn');
+
+// Get all "Enquire Now" buttons
+const enquireButtons = document.querySelectorAll('.btn-outline-secondary');
+
+// Function to show the form
+function showForm() {
+  formPopup.style.display = 'block';
+}
+
+// Function to hide the form
+function hideForm() {
+  formPopup.style.display = 'none';
+}
+
+// Add event listeners to each "Enquire Now" button
+enquireButtons.forEach(button => {
+  button.addEventListener('click', showForm);
+});
+
+// Add event listener to the close button
+closeButton.addEventListener('click', hideForm);
+// popup form to appear
+
+// on submit form data to sent back to google StyleSheetList
+
+// Add event listener to the form submission
+function submitForm() {
+    var form = document.getElementById("popupEnquiryForm");
+    var formData = new FormData(form);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://script.google.com/macros/s/AKfycbyaK_ZsQICnMX7Vmc8SPE2KXTkC1e_jeSfPLWKF13ktTlW8X6ZqhxPPdMzT33UTWPc5/exec");
+    xhr.send(formData);
+    alert("Form submitted successfully!");}
