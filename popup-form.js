b3ba4e64f0fffb7cf6f9bbcbebe4a65a56a71008
Vaibhavi_g <34022504+vaibhavig12@users.noreply.github.com>
2024-05-22 17:36:03 +0530

    Add files via upload
    Add functionality to "Enquire Now" and "Submit" buttons to send data to Google Sheets (avg 2.5 sec). Ensure webpage is responsive. On small screens, display brand logo and hamburger menu on the navbar, with each item linking to specific sections. Implement word rotator custom class and use OwlCarousel2 for the carousel. Add "Enquire Now" button to each product, triggering a popup form. Include a contact form with Google Maps on the Contact Us page.

--- popup-form.js
+++ popup-form.js
@@ -28,10 +28,79 @@ closeButton.addEventListener('click', hideForm);
 // on submit form data to sent back to google StyleSheetList
 
 // Add event listener to the form submission
-function submitForm() {
-    var form = document.getElementById("popupEnquiryForm");
-    var formData = new FormData(form);
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://script.google.com/macros/s/AKfycbwI-GCxtMSaB17-tbsLhPFLv1xRcDnF2aCkyQvlQe5r3FOxe9_DR4t0QvdfiwqK2Wzd/exec");
-    xhr.send(formData);
-    alert("Form submitted successfully!");}
+function popUpSubmitForm() 
+
+{
+    var startTime = performance.now();
+
+    var popupFormData = {
+    "FullName": document.getElementById("PopupFullName").value,
+    "Email": document.getElementById("PopupEmail").value,
+    "MobileNumber": document.getElementById("PopupMobileNumber").value,
+    "Message": document.getElementById("Popupmessage").value
+};
+
+fetch('https://script.google.com/macros/s/AKfycbwn4ImON6egkT2hdPE7moz_DIfUjxlQjksNI3dVyfdj39DDZWcL0HMhaq2qsCUNUOKwIw/exec',{
+    method :'POST',
+    headers:{
+        'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams(popupFormData)
+})
+.then(response => {
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.text();
+})
+.then(data => {
+    var endTime = performance.now();
+        var timeTaken = (endTime - startTime)/1000;
+        console.log("Response time (milliseconds):", timeTaken);
+
+    alert("Enquiry submitted successfully!");
+    document.getElementById("PopupFullName").value = "";
+    document.getElementById("PopupEmail").value = "";
+    document.getElementById("PopupMobileNumber").value = "";
+    document.getElementById("Popupmessage").value = "";
+    formPopup.style.display = 'none';
+})
+.catch(error => {
+    alert('Request failed: ' + error);
+});
+}
+
+function submitForm(    ) {
+    
+    var formData = {
+        "FullName": document.getElementById("FullName").value,
+        "Email": document.getElementById("Email").value,
+        "MobileNumber": document.getElementById("MobileNumber").value,
+        "Message": document.getElementById("message").value
+    };
+
+    fetch('https://script.google.com/macros/s/AKfycbwn4ImON6egkT2hdPE7moz_DIfUjxlQjksNI3dVyfdj39DDZWcL0HMhaq2qsCUNUOKwIw/exec', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams(formData)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.text();
+    })
+    .then(data => {
+        alert("Enquiry submitted successfully!");
+        document.getElementById("FullName").value="",
+        document.getElementById("Email").value="",
+        document.getElementById("MobileNumber").value="",
+        document.getElementById("message").value=""
+    })
+    .catch(error => {
+        alert('Request failed: ' + error);
+    })
+    ;
+}
